4f6f668da74087b5a575db823183c02bffd96879
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-10 13:44:13 +0800

    Add files via upload

--- week2/ch05/main.c
+++ week2/ch05/main.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "string.h"
+int lengthOfLongestSubstring(char * s)
+{
 
+    int len=strlen(s),max=1,num1=1,num2=0,start=0,a=1;
+    if(len==0)
+        return 0;
+    else
+        while(a!=len)
+        {
+            num2=num1;
+            for(int i=0;i<a-start;++i)
+            {
+                if(s[a]==s[start+i])
+                {
+                    start=start+i+1;num1=a-start+1;num2=0;
+                }
+            }
+            if(num1==num2)
+                num1++;
+            if(num1>max)
+                max=num1;
+            ++a;
+        }
+    return max;
+
+}
+int main()
+{
+    char* s="cbdeb";
+
+    int a=lengthOfLongestSubstring(s);
+    printf("%d",a);
+
+    return 0;
+}
