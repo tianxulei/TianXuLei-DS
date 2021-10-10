98660e24e55ab18993f26b13b8b314613bba15b2
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-10 13:51:30 +0800

    11.盛最多水的容器

--- week2/ch12/main.c
+++ week2/ch12/main.c
@@ -1,1 +1,25 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int maxArea(int* height, int heightSize)
+{
+    int start=0,end=heightSize-1,max=0,Max=0,min=0;
+    while(start!=end)
+    {
+        min=(height[start]<height[end])?height[start]:height[end];
+        max=(end-start)*min;
+        if(max>Max)
+            Max=max;
+        if(height[start]>height[end])
+            --end;
+        else
+            ++start;
+    }
+    return Max;
+}
+int main()
+{
+    int a[9]={1,8,6,2,5,4,8,3,7};
+    int b=maxArea(a,9);
+    printf("%d",b);
+    return 0;
+}
