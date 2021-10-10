3b9475b76a71bd480d8b4bf3dc0bd29855ee86db
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-10 13:48:26 +0800

    Add files via upload

--- week2/ch09/main.c
+++ week2/ch09/main.c
@@ -1,1 +1,24 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "limits.h"
+int reverse(int x)
+{
+	long n = 0;
+	while (x)
+	{
+		n = n * 10 + x % 10;
+		x /= 10;
+	}
+	if(n>INT_MAX)
+        return 0;
+    if(n<INT_MIN)
+        return 0;
+	return n;
+}
+int main()
+{
+    int a=123;
+    a=reverse(a);
+    printf("%d",a);
+    return 0;
+}
