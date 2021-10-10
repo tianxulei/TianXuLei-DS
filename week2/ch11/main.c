6529739f6c3cd132a0000a75246e3898867a6218
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-10 13:50:32 +0800

    9.回文数

--- week2/ch11/main.c
+++ week2/ch11/main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+bool isPalindrome(int x)
+{
+    if(x<0 || (x!=0 && x%10==0))
+        return false;
+    else
+    {
+        int a=0;
+        while(a<x)
+        {
+            a=a*10+x%10;
+            x=x/10;
+        }
+        return a==x || x==a/10;
+    }
 
+}
+int main()
+{
+    int a=101;
+    if(isPalindrome(a))
+        printf("yes");
+    else
+        printf("no");
+
+    return 0;
+}
