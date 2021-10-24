46f61f2b89faaff9cdba874c0dc866af1d5fa256
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-24 13:21:43 +0800

    7.23 编写一个程序实现选择算法

--- week4/ch07/main.c
+++ week4/ch07/main.c
@@ -1,1 +1,33 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+//找出第k个最大值
+int Select(int A[], int N, int k)
+{
+	int* nums = calloc(k+1,sizeof(int));
+	for (int i = 0; i < N; ++i)
+	{
+		if (A[i] > nums[k])
+		{
+			nums[k] = A[i];
+			int n = k;
+			while (n > 0 && nums[n] > nums[n - 1])
+			{
+				int Tmp = nums[n];
+				nums[n] = nums[n - 1];
+				nums[n - 1] = Tmp;
+				n--;
+			}
+		}
+	}
+	return nums[k];
+}
+int main()
+{
+	int a[6] = { 5,4,3,2,1,0 };
+	printf("input:");
+	for (int i = 0; i < 6; ++i)
+		printf("%d", a[i]);
+	printf("\noutput:");
+	printf("%d", Select(a,6,3));
+	return 0;
+}
