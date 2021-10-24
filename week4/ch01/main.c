fc439a31a86eb9d420031dda919009436cd8902d
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-24 13:16:17 +0800

    插入排序

--- week4/ch01/main.c
+++ week4/ch01/main.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+void InsertionSort(int* nums, int n)//≤Â»Î≈≈–Ú
+{
+	int tmp,j;
+	for (int i = 1; i < n; ++i)
+	{
+		tmp = nums[i];
+		for (j = i; j > 0 && nums[j - 1] > tmp; --j)
+			nums[j] = nums[j - 1];
+		nums[j] = tmp;
+	}
+}
+int main()
+{
+	int a[5] = { 5,4,3,2,1 };
+	printf("≤Â»Î≈≈–Ú£∫");
+	InsertionSort(a, 5);
+	for (int i = 0; i < 5; ++i)
+		printf("%d", a[i]);
+	return 0;
+}
