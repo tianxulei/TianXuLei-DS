73106972bf12ee8ff66294a8145db93a78f3b30a
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-24 13:17:15 +0800

    希尔排序

--- week4/ch02/main.c
+++ week4/ch02/main.c
@@ -1,1 +1,30 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+void Shellsort(int* nums, int n)//Ï£¶ûÅÅÐò
+{
+	int i, j, Increment, tmp;
+	for (Increment = n / 2; Increment > 0; Increment /= 2)
+	{
+		for (i = Increment; i < n; ++i)
+		{
+			tmp = nums[i];
+			for (j = i; j >= Increment; j -= Increment)
+			{
+				if (tmp < nums[j - Increment])
+					nums[j] = nums[j - Increment];
+				else
+					break;
+			}
+			nums[j] = tmp;
+		}
+	}
+}
+int main()
+{
+	int a[5] = { 5,4,3,2,1 };
+	printf("Ï£¶ûÅÅÐò£º");
+	Shellsort(a, 5);
+	for (int i = 0; i < 5; ++i)
+		printf("%d", a[i]);
+	return 0;
+}
