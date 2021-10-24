0f4525dbc5a93c3406bd8e86fe87fa1d465aa59c
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-24 13:29:02 +0800

    29. 两数相除

--- week4/ch12/main.c
+++ week4/ch12/main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "limits.h"
+int divide(int dividend, int divisor)
+{
+    long long did=dividend,div=divisor;
+    long long quo=0;
+    int  sign=0;
 
+    if( did<0 ){ sign = !sign; did = -did; }
+    if( div<0 ){ sign = !sign; div = -div; }
+
+    for(int i=31;i>=0 && did>=div;--i)
+    {
+        if(did>=(div<<i))
+        {
+            quo=quo+((long long)1<<i);
+            did=did-(div<<i);
+        }
+    }
+    quo = (sign==1) ? -quo : quo;
+    return  ( quo<INT_MIN || quo>INT_MAX ) ? INT_MAX : quo;
+}
+int main()
+{
+    printf("%d",divide(-2147483648,-1));
+    return 0;
+}
