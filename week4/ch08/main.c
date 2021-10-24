1a565189ed7017ec1ff42626ce43798606333e1a
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-24 13:25:19 +0800

    7.32

--- week4/ch08/main.c
+++ week4/ch08/main.c
@@ -1,1 +1,26 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+void Sort(int A[], int N, int true, int maybe, int false)
+{
+	int start = 0;
+	for (int i = 0; i < N; ++i)
+		if (A[i] == true)
+		{
+			A[i] = A[start];
+			A[start++] = true;
+		}
+	for (int i = start; i < N; ++i)
+		if (A[i] == maybe)
+		{
+			A[i] = A[start];
+			A[start++] = maybe;
+		}
+}
+int main()
+{
+	int A[10] = { 9,8,7,6,5,4,3,2,1,0 };
+	Sort(A, 10, 1, 4, 7);
+	for (int i = 0; i < 10; ++i)
+		printf("%d", A[i]);
+	return 0;
+}
