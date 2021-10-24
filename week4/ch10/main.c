e52502a49fd2a3e385f0534b75f3aaa10f16c09f
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-24 13:27:13 +0800

    27. 移除元素

--- week4/ch10/main.c
+++ week4/ch10/main.c
@@ -1,1 +1,22 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int removeElement(int* nums, int numsSize, int val)
+{
+    if(numsSize==0)
+        return 0;
+    int size=numsSize-1;
+    while(size!=-1 && nums[size]==val)
+        --size;
+    for(int i=0;i<size+1;++i)
+        if(nums[i]==val)
+            nums[i--]=nums[size--];
+    return size+1;
+}
+int main()
+{
+    int a[1]={1};
+    int size=removeElement(a,1,1);
+    for(int i=0;i<size;++i)
+        printf("%d",a[i]);
+    return 0;
+}
