76fb9b127d663be14942a184b1c1b17526b1dd64
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-31 17:07:00 +0800

    41. 缺失的第一个正数

--- week5/ch04/main.c
+++ week5/ch04/main.c
@@ -1,1 +1,19 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#define numSize (3)
+int firstMissingPositive(int* nums, int numsSize) 
+{
+	int a[numSize+2] = { 0 };
+	for (int i = 0; i < numsSize; ++i)
+		if (nums[i] > 0 && nums[i] <= numsSize)
+			a[nums[i]] = 1;
+	for (int i = 1; i <= numsSize+1; ++i)
+		if (a[i] == 0)
+			return i;
+}
+int main()
+{
+	int nums[numSize] = { 1,2,0 };
+	printf("%d", firstMissingPositive(nums, numSize));
+	return 0;
+}
