ee1360b16cea9ecc997f59a580bdf7c1b0975c93
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-10-31 17:08:23 +0800

    58. 最后一个单词的长度

--- week5/ch06/main.c
+++ week5/ch06/main.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int lengthOfLastWord(char* s)
+{
+	int len = strlen(s);
+	while (s[len - 1] == ' ')
+		--len;
+	int a = 0;
+	while (len > 0 && s[len - 1] != ' ')
+	{
+		--len; ++a;
+	}
+	return a;
+}
+int main()
+{
+	char a[22] = "luffy is still joyboy";
+	printf("%d", lengthOfLastWord(a));
+	return 0;
+}
