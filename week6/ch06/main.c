b401d92d39e780c89eac780153ea4e96fd2aba03
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-11-07 18:52:00 +0800

    50. Pow(x, n)

--- week6/ch06/main.c
+++ week6/ch06/main.c
@@ -1,1 +1,27 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+double youPow(double x, int n) 
+{
+    if (n == 0)
+        return 1;
+    if (n == 1)
+        return x;
+    if (n % 2)
+        return youPow(x * x, n / 2) * x;
+    else
+        return youPow(x * x, n / 2);
+}
+double myPow(double x, int n)
+{
+    long N = n;
+    if (N < 0)
+        return 1 / youPow(x, -N);
+    else
+        return youPow(x, N);
+}
+int main()
+{
+    printf("%f", myPow(5, 2));
+	return 0;
+}
