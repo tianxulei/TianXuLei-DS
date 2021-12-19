dec19588d0ed97a8fd567645e843c774463261c0
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-12-19 19:21:05 +0800

    Update main.c

--- LeetCode/278. 第一个错误的版本/main.c
+++ LeetCode/278. 第一个错误的版本/main.c
@@ -1,1 +1,16 @@
 
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        unsigned int enter,left=1,right=n;
+        while(left<right)
+        {
+            enter=left+((right-left)>>1);
+            if(isBadVersion(enter))
+                right=enter;
+            else
+                left=enter+1;
+        }
+        return right;
+    }
+};
