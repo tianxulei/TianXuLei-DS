b2f4bdf932a947c14deab896293fd4ee95b9cada
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-12-26 21:19:42 +0800

    Create main.c

---
+++ LeetCode/162. 寻找峰值/main.c
@@ -0,0 +1,9 @@
+int findPeakElement(vector<int>& nums) {
+        int l=0,r=nums.size()-1;
+        while(l<r){
+            int mid=(l+r)/2;
+            if(nums[mid]<nums[mid+1]) l=mid+1;
+            if(nums[mid]>nums[mid+1]) r=mid;
+        }
+        return l;
+    }
