266c85f9c8a49277e03199005e7fe71ef6cbd8be
tianxulei <91713778+tianxulei@users.noreply.github.com>
2021-12-26 21:20:50 +0800

    Create main.c

---
+++ LeetCode/102. 二叉树的层序遍历/main.c
@@ -0,0 +1,22 @@
+vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>nums;
+        if(!root) return nums;
+        queue<TreeNode*>que;
+        que.push(root);
+        int layer=0;
+        while(!que.empty()){
+            int size=que.size();
+            vector<int>a;
+            a.reserve(size);
+            nums.push_back(a);
+            for(int i=0;i<size;++i){
+                TreeNode* p=que.front();
+                que.pop();
+                nums[layer].push_back(p->val);
+                if(p->left) que.push(p->left);
+                if(p->right) que.push(p->right);
+            }
+            ++layer;
+        }
+        return nums;
+    }
