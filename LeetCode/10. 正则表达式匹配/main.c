0bb5aabbe2dcc2fb3706286704cc7836a3fdc710
tianxulei <91713778+tianxulei@users.noreply.github.com>
2022-01-09 19:36:09 +0800

    Create main.c

---
+++ LeetCode/10. 正则表达式匹配/main.c
@@ -0,0 +1,26 @@
+bool match(string& s, string& p, int i, int j) {
+	    if (i == 0) return false;
+	    if (p[j - 1] == '.') return true;
+	    return s[i - 1] == p[j - 1];
+    }
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<int>>dp(m + 1, vector<int>(n + 1));
+        dp[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (match(s, p, i, j - 1)) {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                }
+                else {
+                    if (match(s, p, i, j)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
