d3cfdd277d549d9ba363a6cf6ec3608ba5c39022
tianxulei <91713778+tianxulei@users.noreply.github.com>
2022-01-16 14:51:05 +0800

    Update main.c

--- LeetCode/119. 杨辉三角 II/main.c
+++ LeetCode/119. 杨辉三角 II/main.c
@@ -1,3 +1,10 @@
+给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+
+示例 1:
+输入: rowIndex = 3
+输出: [1,3,3,1]
+
 vector<int> getRow(int rowIndex) {
         vector<vector<int>>dp(2,vector<int>(rowIndex+1,1));
         int a=1,b=0;
